390d024fbbaf3200efced8117e6ff44c490b5ee0
silverskrrrt <141566437+silverskrrrt@users.noreply.github.com>
2023-08-06 16:42:38 +0200

    Update auditapp.py

--- auditapp.py
+++ auditapp.py
@@ -4,32 +4,44 @@ import pandas as pd
 import numpy as np
 
 # Mock data generation
-def generate_mock_data():
+def generate_mock_data(adjustment_factor):
     return {
-        "Carbon Emissions (tons)": np.random.randint(1000, 5000),
-        "Water Usage (kL)": np.random.randint(500, 2000),
+        "Carbon Emissions (tons)": np.random.randint(1000, 5000) * adjustment_factor,
+        "Water Usage (kL)": np.random.randint(500, 2000) * adjustment_factor,
         "Waste Recycled (%)": np.random.randint(50, 99),
-        "Energy Consumption (MWh)": np.random.randint(100, 500),
+        "Energy Consumption (MWh)": np.random.randint(100, 500) * adjustment_factor,
         "Safety Incidents": np.random.randint(0, 10),
-        "Employee Training Hours": np.random.randint(100, 400),
+        "Employee Training Hours": np.random.randint(100, 400) * adjustment_factor,
         "Product Quality Issues": np.random.randint(0, 10),
-        "Stakeholder Engagements": np.random.randint(1, 20)
+        "Stakeholder Engagements": np.random.randint(1, 20) * adjustment_factor
     }
 
 # App title and description
-st.title("EHSQ KPI Dashboard for ESG Improvement")
+st.title("Interactive EHSQ KPI Dashboard for ESG Improvement")
 st.write("""
-This dashboard showcases key performance indicators (KPIs) that are relevant for enhancing a company's Environmental, Social, and Governance (ESG) ratings.
+This dashboard showcases key performance indicators (KPIs) with interactive sliders and visualizations.
 """)
 
-# Generate and display KPIs
-data = generate_mock_data()
+# Slider for adjusting mock data
+adjustment_factor = st.slider("Adjustment Factor for KPIs", 0.5, 2.0, 1.0)
+data = generate_mock_data(adjustment_factor)
 
+# Display KPIs and visualizations
 for kpi, value in data.items():
+    st.subheader(kpi)
     st.metric(label=kpi, value=value)
+    
+    if kpi in ["Carbon Emissions (tons)", "Water Usage (kL)", "Energy Consumption (MWh)", "Employee Training Hours", "Stakeholder Engagements"]:
+        st.bar_chart([value, value / adjustment_factor])
+    
+    elif kpi == "Waste Recycled (%)":
+        st.line_chart([value, value / adjustment_factor])
+    
+    elif kpi in ["Safety Incidents", "Product Quality Issues"]:
+        st.pie_chart([value, 10 - value])
 
 st.write("""
-Note: This dashboard uses mock data. To make it functional for your organization, integrate it with your data sources and customize the KPIs accordingly.
+Note: This dashboard uses mock data for demonstration. To make it functional for your organization, integrate it with your data sources and customize the KPIs accordingly.
 """)
 
 # Run the app: `streamlit run ehsq_dashboard.py`
